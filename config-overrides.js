d950745d9df1072a1c58b369446d5f7ff277201e
Badejo Adebola <adebolabadejo11@gmail.com>
2024-07-26 05:58:42 +0100

    minor edit

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,6 @@ module.exports = function override(config, env) {
   config.resolve.fallback = {
     "assert": require.resolve("assert"),
     "url": require.resolve("url"),
-    // Add other polyfills here if needed
   };
   return config;
 };
